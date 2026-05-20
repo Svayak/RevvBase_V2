eb9586ac8cf421b82bc1a2c3a458301c603dc657
Svayak <nils.apelgren@gmail.com>
2026-05-20 15:09:01 +0200

    Skapa standardanvändare i wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,19 @@
 import os
 os.makedirs('/home/data', exist_ok=True)
-from app import app, init_db
+
+from app import app, init_db, get_db
+from werkzeug.security import generate_password_hash
+
 init_db()
+
+with get_db() as conn:
+    count = conn.execute("SELECT COUNT(*) FROM anvandare").fetchone()[0]
+if count == 0:
+    with get_db() as conn:
+        conn.execute(
+            "INSERT INTO anvandare (username, namn, password_hash, roll) VALUES (?,?,?,?)",
+            ("admin", "Admin", generate_password_hash("verkstad123"), "admin")
+        )
 
 if __name__ == '__main__':
     app.run()
